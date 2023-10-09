d0dbcdde1f51112dd0ed01d8fbe8344707301d8c
Silvio Traversaro <silvio@traversaro.it>
2023-10-09 23:10:31 +0200

    Update bld_cxx.bat

--- recipe/bld_cxx.bat
+++ recipe/bld_cxx.bat
@@ -7,9 +7,10 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
-    -DBUILD_TESTING:BOOL=OFF ^
+    -DBUILD_TESTING:BOOL=ON ^
     -DGZ_ENABLE_RELOCATABLE_INSTALL:BOOL=ON ^
-    -DSKIP_PYBIND11:BOOL=ON ^
+    -DSKIP_PYBIND11:BOOL=OFF ^
+    -DPython3_EXECUTABLE:PATH=%PYTHON% ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
@@ -19,4 +20,8 @@ if errorlevel 1 exit 1
 
 :: Install.
 cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+:: Test.
+ctest --output-on-failure -C Release
 if errorlevel 1 exit 1
