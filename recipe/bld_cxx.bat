660ec4d74d0ecd3c91e84221fd88309c5de70d62
Silvio Traversaro <silvio@traversaro.it>
2023-10-11 19:56:15 +0200

    Update bld_cxx.bat

--- recipe/bld_cxx.bat
+++ recipe/bld_cxx.bat
@@ -2,6 +2,12 @@ rmdir /s /q build
 mkdir build
 cd build
 
+if "%python_impl%" == "pypy" (
+  set SKIP_PYBIND11=True
+) else (
+  set SKIP_PYBIND11=False
+)
+
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -9,7 +15,7 @@ cmake ^
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
     -DBUILD_TESTING:BOOL=OFF ^
     -DGZ_ENABLE_RELOCATABLE_INSTALL:BOOL=ON ^
-    -DSKIP_PYBIND11:BOOL=OFF ^
+    -DSKIP_PYBIND11:BOOL=%SKIP_PYBIND11% ^
     -DPython3_EXECUTABLE:PATH=%PYTHON% ^
     %SRC_DIR%
 if errorlevel 1 exit 1
