a3f9738c62fd6e26f4f24bf90cf73c3acef8fc79
Silvio Traversaro <silvio@traversaro.it>
2024-02-14 23:11:43 +0100

    Update build_cxx.sh

--- recipe/build_cxx.sh
+++ recipe/build_cxx.sh
@@ -9,7 +9,18 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -Dgz-msgs10_PYTHON_INTERPRETER=$BUILD_PREFIX/bin/python -Dgz-msgs10_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -Dgz-msgs10_PROTO_GENERATOR_PLUGIN=$BUILD_PREFIX/bin/gz-msgs10_protoc_plugin"
 fi
 
-export SKIP_PYBIND11=ON  # python bindings are built in build_python.sh
+# We only pass SKIP_PYBIND11 on pypy as for regular Python builds, we want to
+# set SKIP_PYBIND11 to False, as pybind11 is used to compile the gz::sim::systems::PythonSystemLoader 
+# (see "Gazebo Systems written in Python" in https://gazebosim.org/api/sim/8/python_interfaces.html)
+# Python bindings itself are compiled instead in bld_py.bat, so the compilation for those is disabled
+# by the standalone_bindings.patch patch
+# PyPy does not support embedding the interpreter, see 
+# https://github.com/conda-forge/gz-sim-feedstock/pull/26#issuecomment-1755196585
+if [[ "${python_impl}" == "pypy" ]] ; then
+  export SKIP_PYBIND11=ON
+else
+  export SKIP_PYBIND11=OFF
+fi
 
 mkdir build
 cd build
